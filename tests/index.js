bd48f226a661543bcb6c5347f8936879718fb6fe
csenn <csennabaum@gmail.com>
2017-05-17 06:15:34 +0800

    add error test

--- tests/index.js
+++ tests/index.js
@@ -1,17 +1,11 @@
 import { expect } from 'chai'
 import { generateFromClass } from '../src'
-// import simple from './fixtures/simple.json'
+
 import basicSchema from './fixtures/basic_schema.json'
 import propertyVariationsSchema from './fixtures/property_variations_schema.json'
 import inheritanceSchema from './fixtures/inheritance_chain_2_schema.json'
 import linkedNodesSchema from './fixtures/linked_nodes_2_schema.json'
-// import inheritanceOverride from './fixtures/inheritanceOverride.json'
-// import inheritanceOverrideSchema from './fixtures/inheritanceOverrideSchema.json'
-// import excludeParent from './fixtures/excludeParent'
-// import excludeParentSchema from './fixtures/excludeParentSchema'
-// import recursive from './fixtures/recursive'
 import recursionSchema from './fixtures/recursive_schema'
-// import schemaDotOrg from './fixtures/schemaDotOrg'
 
 import basic from 'schesign-graph-examples/graphs/export/basic'
 import propertyVariations from 'schesign-graph-examples/graphs/export/property_variations'
@@ -21,7 +15,20 @@ import recursion from 'schesign-graph-examples/graphs/export/recursion'
 
 const { describe, it } = global
 
-describe('generateJsonSchema', () => {
+describe('generateFromClass()', () => {
+  it('should throw an error when class does not exist', () => {
+    let error
+    try {
+      generateFromClass(
+        basic.graph,
+        'o/tests/basic/master/class/non_existenet_aaa'
+      )
+    } catch (err) {
+      error = err
+    }
+    expect(error.message).to.eql('Could not find class o/tests/basic/master/class/non_existenet_aaa in graph')
+  })
+
   it('should convert basic to a json schema', () => {
     const schema = generateFromClass(
       basic.graph,
@@ -61,39 +68,4 @@ describe('generateJsonSchema', () => {
     )
     expect(schema).to.deep.equal(recursionSchema)
   })
-  // describe('internal graphs', () => {
-  //   it('should convert excludeParent to a json schema', () => {
-  //     const schema = generateFromClass(
-  //       excludeParent.graph,
-  //       'https://www.schesign.com/u/my_user/my_design/0.0.1/class/class2'
-  //     )
-  //     expect(schema).to.deep.equal(excludeParentSchema)
-  //   })
-
-  //   it.skip('should convert inheritanceOverride to a json schema', () => {
-  //     const schema = generateFromClass(
-  //       inheritanceOverride.graph,
-  //       'https://www.schesign.com/u/csenn/test_inheritance_override_1/master/class/class1'
-  //     )
-  //     expect(schema).to.deep.equal(inheritanceOverrideSchema)
-  //   })
-
-  //   it.skip('should convert recursive to a json schema', () => {
-  //     const schema = generateFromClass(
-  //       recursive.graph,
-  //       'https://www.schesign.com/o/tests/recursive/master/class/class1'
-  //     )
-  //     expect(schema).to.deep.equal(recursiveSchema)
-  //     // expect(schema).to.deep.equal(inheritanceOverrideSchema);
-  //   })
-
-  //   it.skip('should convert schemaDotOrg to a json schema', () => {
-  //     const schema = generateFromClass(
-  //       schemaDotOrg.graph,
-  //       'https://www.schesign.com/u/csenn/schemadotorg/master/class/person'
-  //     )
-  //     console.log(JSON.stringify(schema, null, 2))
-  //     // expect(schema).to.deep.equal(inheritanceOverrideSchema);
-  //   })
-  // })
 })
